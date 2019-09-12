e97ef00674b25b1e739fd8b5ad2e2280e44a4933
JackAtomo <diego-cernadas-sexto@hotmail.com>
2019-09-12 19:06:01 +0200

    12/09/2019 v1

--- Proyectos/dias/dia-8/starter_project/www/js/src.js
+++ Proyectos/dias/dia-8/starter_project/www/js/src.js
@@ -1,26 +1,34 @@
 'use strict'
-
+//nombro los arrays a unir
 let nombres = ['Pepe', 'Juan', 'Jose', 'Pedro'];
 let edades = [18, 5, 10, 14];
+//le agrego a rray un valor en su ultima celda
 nombres.push('Lola');
 edades.push(20);
 
+//aqui nombro la variable en la que voy a guardar el resultado
 let resultado;
+//llamo a la funcion y le paso las variables que necesita para trabajar
 resultado = unirArrays(nombres, edades);
+//muestro el resultado en consola
 console.log(resultado);
 
 
-
+//nombro la funcion
 function unirArrays(nombres, edades) {
+    // nombro la variable en la que voy a guardar el resultado
     let resultado = [];
+//miro cual es el array mas largo (esto en un + para cuando los arrays son de diferente largo)
     let largo = edades.length;
     if (nombres.length < edades.length) {
         largo = nombres.length;
     }
+    // aqui creo el for que me va agregando valores al aray resultado  
     for (let cont = 0; cont < largo; cont++) {
         resultado.push({ nombre: nombres[cont] }, { edad: edades[cont] });
         console.log(nombres[cont], edades[cont]);
     }
+    //devuelvo el resultado
     return resultado;
 
 }
